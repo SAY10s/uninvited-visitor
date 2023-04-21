283bcc37068c4b86403b9e9a8a7acc238614c26f
Nikodem Majzner <nikodemmajzner@gmail.com>
2023-04-21 20:40:20 +0200

    LETS GO, SOUNDSgit add .!

--- Assets/Scripts/Phone.cs
+++ Assets/Scripts/Phone.cs
@@ -4,8 +4,15 @@ using UnityEngine;
 
 public class Phone : MonoBehaviour
 {
+    public void Start()
+    {
+        FindObjectOfType<AudioMenager>().Play("Ringtone");
+    }
     public void Interact()
     {
         Debug.Log("RING RING!");
+        FindObjectOfType<AudioMenager>().Stop("Ringtone");
+        FindObjectOfType<AudioMenager>().Play("Dialogue");
+
     }
 }
---
+++ Assets/Scripts/Sound.cs
@@ -0,0 +1,22 @@
+using UnityEngine.Audio;
+using UnityEngine;
+
+[System.Serializable]
+public class Sound
+{
+    public AudioClip clip;
+
+    public string name;
+
+    [Range(0f, 1f)]
+    public float volume; 
+    
+    [Range(0f, 1f)]
+    public float spatialBlend;
+
+    [Range(0.1f, 3f)]
+    public float pitch;
+
+    [HideInInspector]
+    public AudioSource source;
+}

2d8a00b71b8e4545d116c0646ade5b17dcefd539
Nikodem Majzner <nikodemmajzner@gmail.com>
2023-04-22 17:06:57 +0200

    wchodzenie do szafy

--- Assets/Scripts/Wardrobe.cs
+++ Assets/Scripts/Wardrobe.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Wardrobe : MonoBehaviour
+public class Wardrobe : MonoBehaviour, IInteractable
 {
     public GameObject player;
     private bool isInside = false;
@@ -10,11 +10,11 @@ public class Wardrobe : MonoBehaviour
     {
         if (isInside)
         {
-            player.transform.position = new Vector3(5.8f, 0f, 0f);
+            player.transform.position = new Vector3(7.44399977f, -0.00899994373f, 1.43299997f);
         }
         else
         {
-            player.transform.position = new Vector3(7.27f, 0.55f, 0f);
+            player.transform.position = new Vector3(7.49f, 0.059f, 0.53f);
         }
         isInside = !isInside;
         Debug.Log("Wardrobe");

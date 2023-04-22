c3a79f34dcb1e1cf92c8a558fe33b1bab8bb84d6
Nikodem Majzner <nikodemmajzner@gmail.com>
2023-04-22 18:34:04 +0200

    tak, żeby się nie rozwaliło całe. Przed dodawaniem wszystkich mechanik i dźwięków

---
+++ Assets/Scripts/MainDoors.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainDoors : MonoBehaviour
+{
+    public void Start()
+    {
+        StartCoroutine(Waiter());
+    }
+
+    IEnumerator Waiter()
+    {
+        yield return new WaitForSecondsRealtime(10);
+        FindObjectOfType<AudioMenager>().Play("Knocking");
+    }
+}
